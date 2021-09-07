2a539a0453de80d6a0e167860ccffaca53073e85
Jackson Oppenheim <jacksonop@gmail.com>
2021-09-07 17:34:16 -0500

    Getting stores

--- server.go
+++ server.go
@@ -3,8 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "20")
+type PlayerServer struct {
+	store PlayerStore
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func GetPlayerScore(player string) int {
+	if player == "Pepper" {
+		return 20
+	}
+
+	if player == "Floyd" {
+		return 10
+	}
+
+	return 0
 }
